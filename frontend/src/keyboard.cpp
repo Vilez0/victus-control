21ab4f0b26e6465f04bcd6e7021e7945b017e6cd
Edip Hazuri <edip@medip.dev>
2025-10-16 14:28:58 +0300

    feat: replace keyboard on/off toggle with brightness slider

--- frontend/src/keyboard.cpp
+++ frontend/src/keyboard.cpp
@@ -11,19 +11,27 @@ VictusKeyboardControl::VictusKeyboardControl(std::shared_ptr<VictusSocketClient>
 	gtk_widget_set_margin_start(keyboard_page, 20);
 	gtk_widget_set_margin_end(keyboard_page, 20);
 
-	toggle_button = gtk_button_new_with_label("Keyboard: OFF");
+	kbd_brightness_slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 255, 1);
+	gtk_widget_set_hexpand(kbd_brightness_slider, true);
+	kbd_brightness_label = GTK_LABEL(gtk_label_new("Keyboard Brightness: 0%"));
+	kbd_brightness = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
+	gtk_box_append(GTK_BOX(kbd_brightness), kbd_brightness_slider);
+	gtk_box_append(GTK_BOX(kbd_brightness), GTK_WIDGET(kbd_brightness_label));
+
+
 	color_chooser = GTK_COLOR_CHOOSER(gtk_color_chooser_widget_new());
 	apply_button = gtk_button_new_with_label("Apply");
+	gtk_widget_set_hexpand(apply_button, true);
 	current_color_label = GTK_LABEL(gtk_label_new("Current Color: #000000"));
-	current_state_label = GTK_LABEL(gtk_label_new("Current State: OFF"));
+	current_brightness_label = GTK_LABEL(gtk_label_new("Current Brightness: 0%"));
 
-	gtk_box_append(GTK_BOX(keyboard_page), toggle_button);
+	gtk_box_append(GTK_BOX(keyboard_page), GTK_WIDGET(kbd_brightness));
 	gtk_box_append(GTK_BOX(keyboard_page), GTK_WIDGET(color_chooser));
-	gtk_box_append(GTK_BOX(keyboard_page), apply_button);
+	gtk_box_append(GTK_BOX(keyboard_page), GTK_WIDGET(apply_button));
 	gtk_box_append(GTK_BOX(keyboard_page), GTK_WIDGET(current_color_label));
-	gtk_box_append(GTK_BOX(keyboard_page), GTK_WIDGET(current_state_label));
+	gtk_box_append(GTK_BOX(keyboard_page), GTK_WIDGET(current_brightness_label));
 
-	g_signal_connect(toggle_button, "clicked", G_CALLBACK(on_toggle_clicked), this);
+	g_signal_connect(kbd_brightness_slider, "value-changed", G_CALLBACK(on_brightness_changed), this);
 	g_signal_connect(color_chooser, "color-set", G_CALLBACK(on_color_set), this);
 	g_signal_connect(apply_button, "clicked", G_CALLBACK(on_apply_clicked), this);
 
@@ -36,20 +44,6 @@ GtkWidget *VictusKeyboardControl::get_page()
 	return keyboard_page;
 }
 
-void VictusKeyboardControl::update_keyboard_state(bool enabled)
-{
-	std::string command = enabled ? "255" : "0";
-	auto result = socket_client->send_command_async(SET_KBD_BRIGHTNESS, command);
-
-	if (result.get() != "OK")
-	{
-		std::cerr << "Failed to update keyboard state!" << std::endl;
-		return;
-	}
-
-	update_keyboard_state_from_device();
-}
-
 void VictusKeyboardControl::update_keyboard_state_from_device()
 {
 	auto keyboard_state = socket_client->send_command_async(GET_KBD_BRIGHTNESS);
@@ -57,14 +51,27 @@ void VictusKeyboardControl::update_keyboard_state_from_device()
 
 	if (szkeyboard_state.find("ERROR") == std::string::npos)
 	{
-		keyboard_enabled = (szkeyboard_state == "255");
-		gtk_button_set_label(GTK_BUTTON(toggle_button), keyboard_enabled ? "Keyboard: ON" : "Keyboard: OFF");
+		int brightness = std::stoi(szkeyboard_state);
+		if (kbd_brightness_slider) {
+			gtk_range_set_value(GTK_RANGE(kbd_brightness_slider), brightness);
+			gtk_label_set_text(kbd_brightness_label, ("Keyboard Brightness: " + std::to_string((brightness * 100) / 255) + "%").c_str());
+		}
 
-		if (current_state_label)
-			gtk_label_set_text(current_state_label, ("Current State: " + (keyboard_enabled ? std::string("ON") : std::string("OFF"))).c_str());
+		if (current_brightness_label)
+			gtk_label_set_text(current_brightness_label, ("Current Brightness: " + std::to_string((brightness * 100) / 255) + "%").c_str());
+
 	}
 	else
 		std::cerr << "Failed to get current keyboard state!" << std::endl;
+}
+
+void VictusKeyboardControl::update_keyboard_brightness(const int brightness) {
+	printf("Setting brightness to %d\n", brightness);
+	auto brightness_state = socket_client->send_command_async(SET_KBD_BRIGHTNESS, std::to_string(brightness));
+	std::string result = brightness_state.get();
+
+	if (result != "OK")
+		std::cerr << "Failed to update keyboard brightness!: " << result << std::endl;
 }
 
 void VictusKeyboardControl::update_keyboard_color(const GdkRGBA &color)
@@ -80,11 +87,11 @@ void VictusKeyboardControl::update_keyboard_color(const GdkRGBA &color)
 		std::cerr << "Failed to update keyboard color!: " << result << std::endl;
 }
 
-void VictusKeyboardControl::on_toggle_clicked(GtkWidget *widget, gpointer data)
+void VictusKeyboardControl::on_brightness_changed(GtkRange *range, gpointer data)
 {
 	VictusKeyboardControl *self = static_cast<VictusKeyboardControl *>(data);
-	self->keyboard_enabled = !self->keyboard_enabled;
-	gtk_button_set_label(GTK_BUTTON(widget), self->keyboard_enabled ? "Keyboard: ON" : "Keyboard: OFF");
+	int brightness = static_cast<int>(gtk_range_get_value(range));
+	gtk_label_set_text(self->kbd_brightness_label, ("Keyboard Brightness: " + std::to_string((brightness * 100) / 255) + "%").c_str());
 }
 
 void VictusKeyboardControl::on_color_set(GtkColorChooser *widget, gpointer data)
@@ -106,7 +113,8 @@ void VictusKeyboardControl::on_apply_clicked(GtkWidget *widget, gpointer data)
 
 	self->update_keyboard_color(color);
 
-	self->update_keyboard_state(self->keyboard_enabled);
+	int brightness = static_cast<int>(gtk_range_get_value(GTK_RANGE(self->kbd_brightness_slider)));
+	self->update_keyboard_brightness(brightness);
 
 	self->update_keyboard_state_from_device();
 
--- frontend/src/keyboard.hpp
+++ frontend/src/keyboard.hpp
@@ -15,20 +15,22 @@ public:
 	GtkWidget *get_page();
 
 private:
-	GtkWidget *toggle_button;
+	GtkLabel *kbd_brightness_label;
+	GtkWidget *kbd_brightness_slider;
+	GtkWidget *kbd_brightness;
 	GtkColorChooser *color_chooser;
 	GtkWidget *apply_button;
+	GtkWidget *button_box;
 	GtkLabel *current_color_label;
-	GtkLabel *current_state_label;
-
-	bool keyboard_enabled;
+	GtkLabel *current_brightness_label;
 
 	void update_keyboard_state(bool enabled);
 	void update_keyboard_state_from_device();
 
+	void update_keyboard_brightness(const int brightness);
 	void update_keyboard_color(const GdkRGBA &color);
 
-	static void on_toggle_clicked(GtkWidget *widget, gpointer data);
+	static void on_brightness_changed(GtkRange *range, gpointer data);
 	static void on_color_set(GtkColorChooser *widget, gpointer data);
 	static void on_apply_clicked(GtkWidget *widget, gpointer data);
 	static void update_current_color_label(gpointer data);
